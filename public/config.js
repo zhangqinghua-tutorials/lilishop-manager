2158c1ee86882b5e92de378ffbf3976286f05966
zhangqinghua <the.patron.saint.of.science@gmail.com>
2023-02-27 18:15:37 +0800

    change to online

--- public/config.js
+++ public/config.js
@@ -3,10 +3,10 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "http://127.0.0.1:8890",
-      buyer: "http://127.0.0.1:8888",
-      seller: "http://127.0.0.1:8889",
-      manager: "http://127.0.0.1:8887",
+      common: "http://lilishop.easybyte-hk.com/api/common",
+      buyer: "http://lilishop.easybyte-hk.com/api/buyer",
+      seller: "http://lilishop.easybyte-hk.com/api/seller",
+      manager: "http://lilishop.easybyte-hk.com/api/manager",
     },
     API_PROD: {
       common: "https://common-api.pickmall.cn",
