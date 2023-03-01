a8a44a3061287236ef507c00ae402536f55e8eb8
zhangqinghua <the.patron.saint.of.science@gmail.com>
2023-03-01 17:55:01 +0800

    add

--- public/config.js
+++ public/config.js
@@ -9,10 +9,10 @@ var BASE = {
       manager: "http://lilishop.easybyte-hk.com/api",
     },
     API_PROD: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      buyer: "http://lilishop.easybyte-hk.com/api",
+      seller: "http://lilishop.easybyte-hk.com/api",
+      common: "http://lilishop.easybyte-hk.com/api",
+      manager: "http://lilishop.easybyte-hk.com/api",
     },
     /**
      * @description // 跳转买家端地址 pc端
